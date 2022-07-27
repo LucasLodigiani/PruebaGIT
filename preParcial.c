f47234a80e4f91de628e282a55c7282a2b59d05a
lucas <lc45648siete@gmail.com>
2022-07-27 21:19:49 +0200

    Ahora el preParcial tiene un comentario //xd

--- preParcial.c
+++ preParcial.c
@@ -1,6 +1,6 @@
 #include <stdio.h>         //Ejercicio Integrador 5 LC1
 #include <stdlib.h>        //Alumno: Santiago Beron comision 3
-#define TAM 5              //Turno: Noche
+#define TAM 5              //Turno: Noche//xd
 
 struct pedido
 {
